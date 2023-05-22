7dddd44232025362644b983054315849981e8b71
Lachlan Grant <grant.lachlan.j@gmail.com>
2023-05-22 21:58:11 +1000

    Cleanup

--- src/js/home.js
+++ src/js/home.js
@@ -3,16 +3,16 @@ const IMAGES = process.env.PICTURES.split(',');
 const HOME_IMG = process.env.HOME_IMG;
 const BLOG_IMG = process.env.BLOG_IMG;
 
-const HOME_ID = "home-img"
-const BLOG_ID = "blog-img"
-const HOME_BTN = "home-img-btn"
+const HOME_ID = "home-img";
+const BLOG_ID = "blog-img";
+const HOME_BTN = "home-img-btn";
 
 setPicture(HOME_IMG, HOME_ID);
-setPicture(BLOG_IMG, BLOG_ID)
+setPicture(BLOG_IMG, BLOG_ID);
 
 function setPicture(name, id) {
     // Get the image URL from the name
-    var imageUrl = URL + name
+    var imageUrl = URL + name;
 
     try {
         var image = document.getElementById(id);
@@ -28,7 +28,7 @@ function randomHomePicture() {
     var randomIndex = Math.floor(Math.random() * IMAGES.length);
     var imageName = IMAGES[randomIndex];
 
-    setPicture(imageName, HOME_ID)
+    setPicture(imageName, HOME_ID);
 }
 
 document.getElementById(HOME_BTN).addEventListener('click', randomHomePicture);
