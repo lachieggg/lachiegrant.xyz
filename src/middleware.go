3e26376316f5f0f12d6834124bac50119a4c528d
Lachlan Grant <grant.lachlan.j@gmail.com>
2023-10-27 14:32:07 +1100

    Update logging

--- src/middleware.go
+++ src/middleware.go
@@ -1,15 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // middlewareFunc is a general middleware function
 // for all endpoints
 func middlewareFunc(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-        // Log the request
-		if logger != nil && r != nil && r.URL != nil {
-			logger.Printf("Request received for endpoint %s from %s", r.URL.Path, r.RemoteAddr)
+		// Log the request
+		if logger == nil {
+			fmt.Println("Logger is nil")
+		} else if r == nil {
+			logger.Println("Request is nil")
+		} else if r.URL == nil {
+			logger.Printf("URL is nil, remote address: %s\n", r.RemoteAddr)
+		} else {
+			logger.Printf("Request received for endpoint %s from %s\n",
+				r.URL.Path,
+				r.RemoteAddr,
+			)
 		}
-        next(w, r)
-    }
+		next(w, r)
+	}
 }
