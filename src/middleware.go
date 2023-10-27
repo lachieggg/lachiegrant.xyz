80e364a35e04e4059eaee91ba203dcdf7f8c035a
Lachlan Grant <grant.lachlan.j@gmail.com>
2023-10-27 14:28:17 +1100

    Update middleware

--- src/middleware.go
+++ src/middleware.go
@@ -7,7 +7,9 @@ import "net/http"
 func middlewareFunc(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
         // Log the request
-        logger.Printf("Request received for endpoint %s from %s", r.URL.Path, r.RemoteAddr)
+		if logger != nil && r != nil && r.URL != nil {
+			logger.Printf("Request received for endpoint %s from %s", r.URL.Path, r.RemoteAddr)
+		}
         next(w, r)
     }
 }
