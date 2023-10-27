5c53deb80d187f8714873d877bd668a163a19b85
Lachlan Grant <grant.lachlan.j@gmail.com>
2023-10-27 14:45:37 +1100

    Simplify

--- src/middleware.go
+++ src/middleware.go
@@ -3,25 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 // middlewareFunc is a general middleware function
 // for all endpoints
 func middlewareFunc(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+
 		// Log the request
-		if logger == nil {
-			fmt.Println("Logger is nil")
-		} else if r == nil {
-			logger.Println("Request is nil")
-		} else if r.URL == nil {
-			logger.Printf("URL is nil, remote address: %s\n", r.RemoteAddr)
-		} else {
-			logger.Printf("Request received for endpoint %s from %s\n",
-				r.URL.Path,
-				r.RemoteAddr,
-			)
+		if logger == nil || r == nil {
+			fmt.Println("Cannot log request")
+			fmt.Printf("logger: %v\n", logger)
+			fmt.Printf("request: %v\n", r)
+			next(w, r)
+			return
 		}
+
+		remoteAddr := r.Header.Get("X-Real-IP")
+
+		if r.URL == nil {
+			logger.Printf("URL is nil, remote address: %s\n", remoteAddr)
+			r.URL = &url.URL{}
+		}
+
+		logger.Printf("Request received for endpoint %s from %s\n",
+			r.URL.Path,
+			remoteAddr,
+		)
+
 		next(w, r)
 	}
 }
