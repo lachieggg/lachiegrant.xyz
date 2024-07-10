f312610b1c8cb95965b1180246676b1f8f5daf56
Lachlan Grant <grant.lachlan.j@gmail.com>
2024-07-10 20:56:01 +1000

    Update js, update bookmarks

--- src/js/home.js
+++ src/js/home.js
@@ -1,16 +1,24 @@
 const URL = 'images/';
 const IMAGES = process.env.PICTURES.split(',');
-const HOME_IMG = process.env.HOME_IMG;
 const BLOG_IMG = process.env.BLOG_IMG;
-
+const BOOK_IMG = process.env.BOOK_IMG;
+const HOME_IMG = process.env.HOME_IMG;
 const HOME_ID = "home-img";
 const BLOG_ID = "blog-img";
+const BOOK_ID = "book-img";
 const HOME_BTN = "home-img-btn";
 
-setPicture(HOME_IMG, HOME_ID);
-setPicture(BLOG_IMG, BLOG_ID);
+setPicture(HOME_ID,HOME_IMG);
+setPicture(BLOG_ID,BLOG_IMG);
+setPicture(BOOK_ID, BOOK_IMG);
 
-function setPicture(name, id) {
+// setPicture
+// id   -> the id of the element to set 
+// name -> the filename of the image
+function setPicture(id, name) {
+    if (name == "") {
+        name = getRandomImage()
+    }
     // Get the image URL from the name
     var imageUrl = URL + name;
 
@@ -23,17 +31,32 @@ function setPicture(name, id) {
     }
 }
 
-function randomHomePicture() {
-    // Get a random index within the array length
+// getRandomImage returns a random image
+// filename.
+function getRandomImage() {
     var randomIndex = Math.floor(Math.random() * IMAGES.length);
-    var imageName = IMAGES[randomIndex];
-    setPicture(imageName, HOME_ID);
+    return IMAGES[randomIndex]
+}
+
+function setRandomImage(id) {
+    imageName = getRandomImage()
+
+    // Get a random index within the array length
+    setPicture(HOME_ID,);
     if(imageName == "") {
-        randomHomePicture();
+        setRandomImage(id);
+    }
+}
+
+// createRandomImageSetter creates an image
+// setter function for an element id 
+function createRandomImageSetter(id) {
+    return function() {
+        setRandomImage(id) 
     }
 }
 
 var homeButton = document.getElementById(HOME_BTN);
 if(homeButton != null) {
-    homeButton.addEventListener('click', randomHomePicture);
+    homeButton.addEventListener('click', createRandomImageSetter(HOME_ID));
 }
