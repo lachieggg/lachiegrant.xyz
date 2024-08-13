83b77c9fa8132ec577985bbdbcdc440268f34715
Lachlan Grant <grant.lachlan.j@gmail.com>
2024-08-13 22:31:48 +1000

    Update logging

--- src/log.go
+++ src/log.go
@@ -7,8 +7,17 @@ import (
 
 var logger *log.Logger
 
+const (
+	loggingKey = "LOGGING"
+)
+
 // initLogger
 func initLogger(logFileName string) {
+	_, logging := os.LookupEnv(loggingKey)
+	if !logging {
+		return
+	}
+
 	logFile, err := os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
