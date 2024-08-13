509b919539b79123809f15fce5e85e9924914e30
Lachlan Grant <grant.lachlan.j@gmail.com>
2024-08-13 22:37:42 +1000

    Update logging

--- src/middleware.go
+++ src/middleware.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"net/url"
 )
@@ -10,20 +9,9 @@ import (
 // for all endpoints
 func middlewareFunc(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
-		// Log the request
-		if logger == nil || r == nil {
-			fmt.Println("Cannot log request")
-			fmt.Printf("logger: %v\n", logger)
-			fmt.Printf("request: %v\n", r)
-			next(w, r)
-			return
-		}
-
 		remoteAddr := r.Header.Get("X-Real-IP")
 
 		if r.URL == nil {
-			logger.Printf("URL is nil, remote address: %s\n", remoteAddr)
 			r.URL = &url.URL{}
 		}
 
