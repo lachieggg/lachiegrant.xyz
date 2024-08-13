8747bd7bf0159d61abcbfedfbd125c2d94e75178
Lachlan Grant <grant.lachlan.j@gmail.com>
2024-08-13 20:37:23 +1000

    Update scripts/certbot.sh

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+current_dir="$(basename "$PWD")"
+
+# Check if we are in the root project folder
+if [ "$current_dir" = "scripts" ] || [ ! -f "Makefile" ]; then
+    echo "Must be run from the root project folder."
+    exit;
+fi
+
 # Prompt the user for input
-echo -n "Do you want to kill the service? (Y/n) "
+echo -n "Do you want to kill the service? (y/n) "
 read input
 
 # Convert the input to uppercase
@@ -17,6 +25,30 @@ else
     exit;
 fi
 
+# Backup
+echo "Backing up..."
+sudo mkdir -p /etc/letsencrypt/live/backup
+sudo rsync -av --exclude='backup/' /etc/letsencrypt/live/ /etc/letsencrypt/live/backup/
+
+# Delete old certs
+echo "Deleting old certs..."
+sudo find /etc/letsencrypt/live/ -mindepth 1 -maxdepth 1 ! -name 'backup' -exec rm -rf {} +
+sudo find /etc/letsencrypt/live/
+
+# Update certificate
+echo "Updating certificates"
 sudo certbot certonly --standalone -d lachiegrant.xyz -d www.lachiegrant.xyz
 
+# Move cert to default folder
+echo "Move certificate to default folder"
+sudo find /etc/letsencrypt/live/ -mindepth 1 -maxdepth 1 -name 'lachiegrant.xyz*' \
+	-exec mv {} /etc/letsencrypt/live/lachiegrant.xyz/ \;
+
+# Copy new certificate to web server configuration folder
+echo "Copying new certificate to web server"
+sudo cp /etc/letsencrypt/live/lachiegrant.xyz/fullchain.pem ./tls/nginx/certs/fullchain.pem
+sudo cp /etc/letsencrypt/live/lachiegrant.xyz/privkey.pem ./tls/nginx/keys/privkey.pem
+sudo find /etc/letsencrypt/live
+
+# Restart service
 docker-compose up -d
